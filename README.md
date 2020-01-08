31743f1cc0b72c7513026fe47c52e96e6e334cbf
GasinAn <59331299+GasinAn@users.noreply.github.com>
2020-01-09 06:53:47 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ต⦕⦁.⦁⦖ต Easy-BNU-Connector 0.2.0
+# ต⦕⦁.⦁⦖ต Easy-BNU-Connector 0.2.1
 
 连接北京师范大学校园网的 Python 程序
 
@@ -8,20 +8,25 @@
 
 1. 点击 “Clone or download” 后点击 “Download ZIP” 下载，
 
-2. 解压 Easy-BNU-Connector-0.2.0.zip，
+2. 解压 Easy-BNU-Connector-0.2.1.zip，
 
-3. 双击 Easy-BNU-Connector-0.2.0 文件夹内的 setup.bat，
+3. 双击 Easy-BNU-Connector-0.2.1 文件夹内的 setup.bat，
 
 此后会开始安装并出现窗口。安装过程中，若窗口中出现 “Proceed (\[y]/n)?” 字样，需要按回车键以确认安装。
 
-无论是否安装成功，窗口最终都会自动关闭。若窗口关闭后，Easy-BNU-Connector-0.2.0.zip 和 Easy-BNU-Connector-0.2.0 文件夹消失，并出现 Easy-BNU-Connector.bat，说明安装成功，否则安装失败。若安装失败，请确认网络连接状况良好后重新安装。
+无论是否安装成功，窗口最终都会自动关闭。若窗口关闭后，Easy-BNU-Connector-0.2.1.zip 和 Easy-BNU-Connector-0.2.1 文件夹消失，并出现 Easy-BNU-Connector.bat，说明安装成功，否则安装失败。若安装失败，请确认网络连接状况良好后重新安装。
 
 以后双击 Easy-BNU-Connector.bat 即可使用。
 
 ## 警告
 
 安装后，注册表内 HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\NlaSvc\Parameters\Internet 中 EnableActiveProbing 的值会被修改为 0。这一改变将使 Windows 不再自动检查是否成功连接网络。对于 Windows 8 和 Windows 10 系统，这意味着浏览器在连接校园网后不会自启动并出现校园网登录界面。
+
 若要恢复，把 EnableActiveProbing 的值修改为 1 即可。
+
+## 声明
+
+安装时可能会添加 WLAN 配置文件，这对不使用 Easy-BNU-Connector 的情况下连接校园网没有影响。
 
 ## 鸣谢
 
@@ -29,23 +34,13 @@
 
 ## 更新
 
-1. 开始采用 ต⦕⦁.⦁⦖ต 为 LOGO；
+1. 完善安装机制，使之更加方便快捷省流量；
 
-2. 暂时放弃跨平台，转而全力保证 Windows 系统中的良好运作；
-
-3. 优化图形界面，解决界面不居中显示的问题；
-
-4. 解决 BNU 无法连接的问题，增加持续检查网络状况的机制；
-
-5. 完善阻止浏览器自启动的机制；
-
-6. 完善安装机制，使之更加方便；
-
-7. 改进程序结构，增加基本注释，使程序更加清晰易懂。
+2. 解决从未连接过 BNU-Student 或 BNU 的用户不能正常使用的问题。
 
 ## 特别说明
 
-目前添加了设置按钮，但并未给这个按钮配置任何功能。
+目前图形界面内有“设置”按钮，但尚未给这个按钮配置任何功能。
 
 ## 未来计划
 
@@ -63,13 +58,8 @@
 
 ……
 
-
 ## 不予考虑的事
 
 采用严重不符合规范的代码；
 
-不使用认证网关内的 JavaScript 和由认证网关内的 JavaScript 转换成的 Python 程序。
-
-## 注释
-
-methods.py 是由 methods.js 通过 Js2Py 转换成的，methods.js 取自认证网关。程序运行不需有 methods.js 。
+不使用认证网关内的 JavaScript。
--- setup.py
+++ setup.py
@@ -1,40 +1,92 @@
 # Author: GasinAn
 
+import sys
+assert (sys.platform in {'win32', 'cygwin'})
 
 import os
-import sys
+import time
+try:
+    import _thread
+except:
+    import thread as _thread
+try:
+    import tkinter
+except:
+    import Tkinter as tkinter
 
+try:
+    from requests import request
+except:
+    print('Installing Requests...')
+    if os.system('conda install requests') is 0:
+        pass
+    elif os.system('pip install requests') is 0:
+        pass
+    else:
+        raise OSError
+    from requests import request
 
-print('Installing Requests and BeautifulSoup4...')
-if os.system('conda install requests beautifulsoup4') is 0:
-    pass
-elif os.system('pip install requests beautifulsoup4') is 0:
-    pass
-else:
-    raise OSError
+try:
+    from bs4 import BeautifulSoup
+except:
+    print('Installing Beautifulsoup4...')
+    if os.system('conda install beautifulsoup4') is 0:
+        pass
+    elif os.system('pip install beautifulsoup4') is 0:
+        pass
+    else:
+        raise OSError
+    from bs4 import BeautifulSoup
 
-print('Installing Js2Py...')
-if os.system('pip install js2py') is 0:
-    pass
-else:
-    raise OSError
+try:
+    from js2py import translate_file as trans
+except:
+    print('Installing Js2Py...')
+    if os.system('pip install js2py') is 0:
+        pass
+    else:
+        raise OSError
+    from js2py import translate_file as trans
 
 print('Setting up...')
 for path in sys.path:
     if path[-13:] == 'site-packages':
         easy_bnu_connector_path = path+'\\easy_bnu_connector'
         break
+
+trans('easy_bnu_connector\\methods.js', 'easy_bnu_connector\\methods.py')
 with open('easy_bnu_connector\\Easy-BNU-Connector.bat', 'w') as f:
-    f.write('python '+easy_bnu_connector_path+'\\login.py\t\nexit')
+    try:
+        f.write('python '+easy_bnu_connector_path+'\\login.py\t\nexit')
+    except:
+        easy_bnu_connector_path = sys.path[0]+'\\easy_bnu_connector'
+        f.write('python '+easy_bnu_connector_path+'\\login.py\t\nexit')
+with open('easy_bnu_connector\\warmup.py', 'w') as f:
+    f.write('from methods import methods')
+
 os.system('mkdir '+easy_bnu_connector_path)
-os.system('copy /y easy_bnu_connector\\* '+easy_bnu_connector_path)
+os.system('move /y easy_bnu_connector\\* '+easy_bnu_connector_path)
+os.system('python '+easy_bnu_connector_path+'\\warmup.py')
+
+def add_profile(netname, ssid, xml):
+    with open('WLAN-'+netname+'.xml', 'w') as f:
+        f.write(xml.replace('netname', netname).replace('ssid', ssid))
+    os.system('netsh wlan add profile filename="WLAN-'+netname+'.xml"')
+
+with open('setup.xml') as f:
+    xml = f.read().replace('\n', '\t\n')
+add_profile('BNU-Student', '424E552D53747564656E74', xml)
+add_profile('BNU', '424E55', xml)
+
+def change_regedit(header, reg):
+    with open(header.replace(' ', '-')+'.reg', 'w') as f:
+        f.write(reg.replace('HEADER', header))
+    os.system('regedit /s '+sys.path[0]+'\\'+header.replace(' ', '-')+'.reg')
+
+with open('setup.reg') as f:
+    reg = f.read().replace('\n', '\t\n')
+change_regedit('Windows Registry Editor Version 5.00', reg)
+change_regedit('REGISTRY4', reg)
 
 with open('..\\Easy-BNU-Connector.bat', 'w') as f:
     f.write('start /min '+easy_bnu_connector_path+'\\Easy-BNU-Connector.bat')
-
-with open('setup.reg', 'w') as f:
-    f.write('Windows Registry Editor Version 5.00\t\n\t\n')
-    f.wrtie('[HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Services')
-    f.write('\\NlaSvc\\Parameters\\Internet]\t\n')
-    f.wrtie('"EnableActiveProbing"=dword:00000000')
-os.system('regedit /s '+sys.path[0]+'\\setup.reg')
