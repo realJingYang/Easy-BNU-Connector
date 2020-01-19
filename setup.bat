b7108cd52d651a0b0805c4113cee3595ff52e3e2
GasinAn <Gasin185@163.com>
2020-01-19 13:32:30 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 python setup.py
 chdir ..
 del Easy-BNU-Connector*
-move /y Easy-BNU-Connector-0.2.2\Easy-BNU-Connector-0.2.2.vbs
-rmdir /s /q Easy-BNU-Connector-0.2.2
+move /y Easy-BNU-Connector-0.2.3\Easy-BNU-Connector-0.2.3.vbs
+rmdir /s /q Easy-BNU-Connector-0.2.3
 exit
